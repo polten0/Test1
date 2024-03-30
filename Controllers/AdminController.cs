425639523ba1c406851d38b9b65ed6617fc490cd
polten0 <alexey.mikryukov@mail.ru>
2024-03-30 20:02:31 +0300

    Admin Contorller

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> userManager;
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View(this.userManager.);
+        }
+    }
+}
